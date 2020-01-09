dbdfc37696f2e97856fb03120bed77b0637bb9ab
Batatos <ahedi@amdocs.com>
2020-01-09 20:42:38 +0200

    Updates

---
+++ precache-manifest.d8601aae895d7bcdd9b4dff0a893b696.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "54abc83bbef0f4435c636a22616f9143",
+    "url": "/portfolio/index.html"
+  },
+  {
+    "revision": "cc5cae51e85e9b1a7f19",
+    "url": "/portfolio/static/css/main.eabf265c.chunk.css"
+  },
+  {
+    "revision": "9bc206a4c92299f42db1",
+    "url": "/portfolio/static/js/2.6bd413a1.chunk.js"
+  },
+  {
+    "revision": "d705cb622423d72c5defbf368ca70dcc",
+    "url": "/portfolio/static/js/2.6bd413a1.chunk.js.LICENSE"
+  },
+  {
+    "revision": "cc5cae51e85e9b1a7f19",
+    "url": "/portfolio/static/js/main.933cf9f2.chunk.js"
+  },
+  {
+    "revision": "e139b2548d94e481a5d0",
+    "url": "/portfolio/static/js/runtime-main.0762ecda.js"
+  },
+  {
+    "revision": "80851b30567570a3aa4d7ee0d011b900",
+    "url": "/portfolio/static/media/mylogo.80851b30.gif"
+  }
+]);
