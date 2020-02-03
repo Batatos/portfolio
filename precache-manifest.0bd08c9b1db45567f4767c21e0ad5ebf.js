ca547ba8e7ec75023027ea7def22948e1c24245f
Batatos <ahedi@amdocs.com>
2020-02-03 21:20:02 +0200

    Updates

--- precache-manifest.0bd08c9b1db45567f4767c21e0ad5ebf.js
+++ precache-manifest.0bd08c9b1db45567f4767c21e0ad5ebf.js
@@ -1,27 +1,27 @@
 self.__precacheManifest = (self.__precacheManifest || []).concat([
   {
-    "revision": "233d80972c67c229c445a59bb535da22",
+    "revision": "0554c5b19996d2197c5c44237cd0dae1",
     "url": "/portfolio/index.html"
   },
   {
-    "revision": "58440096a6c0890c7e0c",
+    "revision": "8ba8a1d668cc5dd5f00b",
     "url": "/portfolio/static/css/2.47e06e2e.chunk.css"
   },
   {
-    "revision": "33c48342cdcc776c147f",
-    "url": "/portfolio/static/css/main.5c39b36a.chunk.css"
+    "revision": "0dc5683a66bbe601a9ba",
+    "url": "/portfolio/static/css/main.9bb43bb7.chunk.css"
   },
   {
-    "revision": "58440096a6c0890c7e0c",
-    "url": "/portfolio/static/js/2.6f4c84dc.chunk.js"
+    "revision": "8ba8a1d668cc5dd5f00b",
+    "url": "/portfolio/static/js/2.b6887401.chunk.js"
   },
   {
     "revision": "f032203ca460334c00de541c30a6078a",
-    "url": "/portfolio/static/js/2.6f4c84dc.chunk.js.LICENSE"
+    "url": "/portfolio/static/js/2.b6887401.chunk.js.LICENSE"
   },
   {
-    "revision": "33c48342cdcc776c147f",
-    "url": "/portfolio/static/js/main.81b417db.chunk.js"
+    "revision": "0dc5683a66bbe601a9ba",
+    "url": "/portfolio/static/js/main.48ffd90d.chunk.js"
   },
   {
     "revision": "0d98471f6c3b69ca4251",
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/portfolio/precache-manifest.bef3d7123ce15054d7040b52fc0956db.js"
+  "/portfolio/precache-manifest.0bd08c9b1db45567f4767c21e0ad5ebf.js"
 );
 
 self.addEventListener('message', (event) => {
